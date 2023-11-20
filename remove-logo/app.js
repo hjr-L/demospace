686c447808d04b7cbe051e1a2bb9c9c704a046a0
unknown <renee.huang@ibaiqiu.com>
2023-11-20 18:39:42 +0800

    remove-logo

--- remove-logo/app.js
+++ remove-logo/app.js
@@ -1,12 +1,40 @@
 // app.js
-import {silentLogin} from './utils/login'
+import {
+    silentLogin
+} from './utils/login'
 App({
-  onLaunch() {
+    globalData: {
+        userInfo: null,
+        navHeight: 0,
+        barHeight: 0,
+        ios: true,
+        safeAreaTop: 0,
+        navlLeftWidth: 0,
+        navInnerPaddingRight: 0
+    },
+    onLaunch() {
 
-    // 登录
-    silentLogin()
-  },
-  globalData: {
-    userInfo: null
-  }
+        // 静默登录
+        silentLogin();
+        const rect = wx.getMenuButtonBoundingClientRect()
+        wx.getSystemInfo({
+            success: (res) => {
+                const isAndroid = res.platform === 'android'
+                const isDevtools = res.platform === 'devtools'
+                let statusBarHeight = res.statusBarHeight // 设备顶部标签栏信息
+                let navHeight
+                if (rect) {
+                    let navTop = rect.top // 胶囊按钮与顶部的距离
+                    navHeight = statusBarHeight + rect.height + (navTop - statusBarHeight) * 3 // 导航高度
+                } else {
+                    navHeight = statusBarHeight + 51
+                }
+                this.globalData.ios = !isAndroid;
+                this.globalData.navHeight = navHeight;
+                this.globalData.navInnerPaddingRight = `padding-right: ${res.windowWidth - rect.left}px`;
+                this.globalData.navlLeftWidth = `width: ${res.windowWidth - rect.left }px`;
+                this.globalData.safeAreaTop = isDevtools || isAndroid ? `height: calc(var(--height) + ${res.safeArea.top}px); padding-top: ${res.safeArea.top}px` : ``;
+            }
+        })
+    }
 })
---
+++ remove-logo/pages/user/components/memberInfo/memberInfo.js
@@ -0,0 +1,22 @@
+Component({
+    options: {
+        styleIsolation: "apply-shared"
+    },
+
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+
+    },
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
